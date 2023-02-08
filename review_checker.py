d03f7809eb686bab18f2c4005c18b26a7103611c
Imranbek Aliev <imranbek.aliev@zoodmall.com>
2023-02-08 16:30:42 +0300

    comments deleted

--- review_checker.py
+++ review_checker.py
@@ -22,19 +22,17 @@ def main():
     bot = DvmnBot()
     while True:
         try:
-            # print('New req ' + str(timestamp))
             checking_body = ask_for_events(timestamp=timestamp if timestamp else None)
 
             if checking_body['status'] == 'timeout':
-                # print('No events. Retrying.')
                 timestamp = {'timestamp': checking_body['timestamp_to_request']}
-            elif checking_body['status'] == 'found':
-                # print(json.dumps(checking_body, indent=1))
 
+            elif checking_body['status'] == 'found':
                 review_info = checking_body['new_attempts'][0]
                 bot.send_review_message(chat_id, review_info)
 
                 timestamp = {'timestamp': checking_body['last_attempt_timestamp']}
+
         except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
             continue
 

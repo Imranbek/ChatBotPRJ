5b95d47b0a50339593357e7b9a22364aacfd8685
Imranbek Aliev <imranbek.aliev@zoodmall.com>
2023-02-09 17:55:49 +0300

    argparce added. Variables name changed

--- review_checker.py
+++ review_checker.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import time
 from random import choice
@@ -7,41 +8,39 @@ import telegram
 from dotenv import load_dotenv
 
 
-
 def main():
+    load_dotenv()
     dvmn_url = 'https://dvmn.org/api/long_polling/'
     dvmn_token = os.environ['DVMN_TOKEN']
     headers = {
         'Authorization': f'Token {dvmn_token}'
     }
-
-    chat_id = input("Please enter your chat_id:\n")
+    bot = turn_on_bot()
+    chat_id = parce_chat_id()
     print('Start looking for new reviews')
     timestamp = {}
 
     while True:
         try:
-            checking_new_event = requests.get(dvmn_url, headers=headers, timeout=5, params=timestamp)
-            checking_new_event.raise_for_status()
-            checking_body = checking_new_event.json()
+            new_event_response = requests.get(dvmn_url, headers=headers, timeout=5, params=timestamp)
+            new_event_response.raise_for_status()
+            event_state = new_event_response.json()
 
-            if checking_body['status'] == 'timeout':
-                timestamp = {'timestamp': checking_body['timestamp_to_request']}
+            if event_state['status'] == 'timeout':
+                timestamp = {'timestamp': event_state['timestamp_to_request']}
 
-            elif checking_body['status'] == 'found':
-                review_info = checking_body['new_attempts'][0]
-                send_review_message(chat_id, review_info)
+            elif event_state['status'] == 'found':
+                review_info = event_state['new_attempts'][0]
+                send_review_message(bot, chat_id, review_info)
 
-                timestamp = {'timestamp': checking_body['last_attempt_timestamp']}
+                timestamp = {'timestamp': event_state['last_attempt_timestamp']}
 
         except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
             time.sleep(60)
             continue
 
 
-def send_review_message(chat_id, review_info):
-    token = os.environ['BOT_TOKEN']
-    bot = telegram.Bot(token=token)
+def send_review_message(bot, chat_id, review_info):
     message_positive = 'Поздравляем, Вы проделали отличную работу! Преподавателю все понравилось, можете приступать к следующему уроку!'
     message_negative = ['К сожалению, в работе нашлись ошибки, которые необходимо исправить. Удачи на следующем ревью! Мы верим в Вас!',
                         'В работе нашлись ошибки. Исправьте их и попробуйте еще раз =) Удачи!',
@@ -55,12 +54,27 @@ def send_review_message(chat_id, review_info):
     message_text = f'''Преподаватель проверил Вашу работу по уроку \'{lesson_title}\'.
 
         {review_result}
-        -------' \
+        -------
         Ссылка на урок: {lesson_url}'''
 
     bot.send_message(chat_id=chat_id, text=message_text)
 
 
+def turn_on_bot():
+    token = os.environ['BOT_TOKEN']
+    bot = telegram.Bot(token=token)
+    return bot
+
+
+def parce_chat_id():
+    parser = argparse.ArgumentParser(
+        description='DVMN review controller'
+    )
+    parser.add_argument('chid', help='Ваш chat_id')
+    args = parser.parse_args()
+
+    return args.chid
+
+
 if __name__ == '__main__':
-    load_dotenv()
     main()
